1bc848e8e260ed10369c202862ccd6793b4b0c94
Claudio <diolclaudio>
2021-02-17 17:28:32 +0200

    update settings

--- pay/settings.py
+++ pay/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'njmgi=ex+=p1lq^4r0$ue(ea9k-!037ezdz05$a$s7@mtr*n74'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['diol.herokuapp.com', 'localhost', 'http://127.0.0.1:8000/']
 
 
 # App1\lication definition
